cc7e32104931fd6d1928c1a52f500fa34e0f4257
Arizal Akbar Zikri <akbarpn136@gmail.com>
2020-06-27 03:12:40 +0700

    menerapkan dotenv

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,21 @@
 mod routers;
 
 use std::io;
+use std::env;
 use actix_web::{HttpServer, App, web, HttpResponse};
 
 #[actix_rt::main]
 async fn main() -> io::Result<()> {
+    dotenv::dotenv().ok();
+
+    let address = format!("{}:{}",
+                          env::var("APP_HOST").expect("variabel HOST perlu didefiniskan"),
+                          env::var("APP_PORT").expect("variabel PORT perlu didefiniskan"));
     let server = HttpServer::new(|| {
         App::new()
             .route("/", web::get().to(|| HttpResponse::Ok().body("Halaman ini dikosongkan")))
             .configure(routers::config)
-    }).bind("localhost:8080").unwrap();
+    }).bind(address).unwrap();
 
     println!("Menjalankan server...");
     server.run().await
