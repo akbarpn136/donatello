a25e81798da08590132421c65a488af62fad5d02
Arizal Akbar Zikri <akbarpn136@gmail.com>
2020-07-03 21:39:46 +0700

    terapkan env_logger init

--- src/main.rs
+++ src/main.rs
@@ -2,17 +2,19 @@ mod routers;
 
 use std::io;
 use std::env;
-use actix_web::{HttpServer, App, web, HttpResponse};
+use actix_web::{HttpServer, App, web, middleware, HttpResponse};
 
 #[actix_rt::main]
 async fn main() -> io::Result<()> {
     dotenv::dotenv().ok();
+    env_logger::init();
 
     let address = format!("{}:{}",
-                          env::var("APP_HOST").expect("variabel HOST perlu didefiniskan"),
-                          env::var("APP_PORT").expect("variabel PORT perlu didefiniskan"));
+                          env::var("APP_HOST").expect("variabel HOST perlu didefinisikan"),
+                          env::var("APP_PORT").expect("variabel PORT perlu didefinisikan"));
     let server = HttpServer::new(|| {
         App::new()
+            .wrap(middleware::Logger::default())
             .route("/", web::get().to(|| HttpResponse::Ok().body("Halaman ini dikosongkan")))
             .configure(routers::config)
     }).bind(address).unwrap();
