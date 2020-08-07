950f9fc40f155aae92cc34984235a40f53fd09d4
Arizal Akbar ZIkri <akbarpn136@gmail.com>
2020-08-07 20:23:10 +0700

    menerapkan state kedalam route

--- src/user/routers.rs
+++ src/user/routers.rs
@@ -2,9 +2,11 @@ use actix_web::{web, HttpResponse};
 
 use crate::user::model::User;
 use crate::user::interface::Info;
+use crate::state::AppState;
 
 pub fn config(cfg: &mut web::ServiceConfig) {
-    cfg.route("/", web::get().to(|| {
+    cfg.route("/", web::get().to(|state: web::Data<AppState>| {
+            let app_name = &state.app_name;
             let user = User {
                 nama: String::from("Fulan"),
                 email: String::from("fulan@gmail.com")
@@ -16,19 +18,30 @@ pub fn config(cfg: &mut web::ServiceConfig) {
             println!("{:?}", user);
             println!("{}", user_tostring);
             println!("{:?}", user_fromstring);
+            println!("Nama aplikasi ini: {}", app_name);
 
             HttpResponse::Ok().body("ini versi 1 untuk user")
         }))
-        .route("/{id}/", web::get().to(|id: web::Path<String>, informasi: Option<web::Query<Info>>| {
+        .route("/{id}/", web::get().to(|id: web::Path<String>,
+                                        informasi: Option<web::Query<Info>>,
+                                        state: web::Data<AppState>| {
+            let app_name = &state.app_name;
             let body = if informasi.is_none() {
-                format!("User id: {}", id)
+                format!("User id: {} di aplikasi {}", id, app_name)
             } else {
-                format!("User id: {} dengan kelompok {}", id, informasi.unwrap().kelompok)
+                format!("User id: {} dengan kelompok {} di aplikasi {}", id,
+                        informasi.unwrap().kelompok,
+                        app_name)
             };
 
             HttpResponse::Ok().body(body)
         }))
-        .route("/", web::post().to(|payload: web::Form<User>| {
-            HttpResponse::Created().body(format!("User {} denagn email {} berhasil dibuat", payload.nama, payload.email))
+        .route("/", web::post().to(|payload: web::Form<User>,
+                                    state: web::Data<AppState>| {
+            let app_name = &state.app_name;
+            HttpResponse::Created().body(
+                format!("User {} denagn email {} berhasil dibuat melalui {}", payload.nama,
+                        payload.email, app_name)
+            )
         }));
 }
