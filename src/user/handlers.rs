4551f80daf30413a8da507a78ad5637012d15bc3
userbaruu <fulan.gulan@gmail.com>
2020-11-08 06:53:23 +0700

    buat handlers untuk ambil data user

--- src/user/handlers.rs
+++ src/user/handlers.rs
@@ -1,9 +1,8 @@
 use uuid::Uuid;
 use bcrypt::hash;
-use actix_web::{HttpResponse, web, Error};
+use actix_web::{HttpResponse, web, Error, ResponseError};
 use diesel::r2d2::{Pool, ConnectionManager};
 use diesel::{SqliteConnection, insert_into, RunQueryDsl};
-use actix_web::rt::blocking::BlockingError;
 
 use crate::user::model::User;
 use crate::schema::users::dsl::*;
@@ -31,11 +30,21 @@ pub async fn tambah_user(payload: web::Form<UserBaru>, pool: web::Data<DbPool>) 
 
         Ok(user_baru)
     }).await.map_err(|err| {
-        match err {
-            BlockingError::Error(e) => HttpResponse::BadRequest().body(e.to_string()),
-            BlockingError::Canceled => HttpResponse::InternalServerError().body("Cancled")
-        }
+        err.error_response()
     })?;
 
     Ok(HttpResponse::Created().json(user))
+}
+
+pub async fn ambil_user(pool: web::Data<DbPool>) -> Result<HttpResponse, Error> {
+    let conn = pool.get().unwrap();
+    let user = web::block(move || -> Result<Vec<User>, diesel::result::Error> {
+        let lihat_user = users.load::<User>(&conn)?;
+
+        Ok(lihat_user)
+    }).await.map_err(|e| {
+        e.error_response()
+    })?;
+
+    Ok(HttpResponse::Ok().json(user))
 }
