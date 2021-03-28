242ef60dd6eae3da342060cb1cca616a8eb7de10
user.baruu <fulan.gulan@gmail.com>
2021-03-28 07:49:26 +0700

    Belajar rust seputar collections

--- exercises/collections/hashmap2.rs
+++ exercises/collections/hashmap2.rs
@@ -12,7 +12,6 @@
 // Execute the command `rustlings hint collections4` if you need
 // hints.
 
-// I AM NOT DONE
 
 use std::collections::HashMap;
 
@@ -38,6 +37,11 @@ fn fruit_basket(basket: &mut HashMap<Fruit, u32>) {
         // TODO: Put new fruits if not already present. Note that you
         // are not allowed to put any type of fruit that's already
         // present!
+        match fruit {
+            Fruit::Banana => { basket.insert(fruit, 2); }
+            Fruit::Pineapple => { basket.insert(fruit, 3); }
+            _ => ()
+        }
     }
 }
 
